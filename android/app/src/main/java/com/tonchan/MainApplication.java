0a317788e494a668c107cc4fbd85579d31f2b355
Your Name <you@example.com>
2025-10-28 08:51:50 +0000

    Wallet funzionante - versione aggiornata e stabile

--- android/app/src/main/java/com/tonchan/MainApplication.java
+++ android/app/src/main/java/com/tonchan/MainApplication.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -16,15 +14,13 @@ import com.facebook.react.modules.network.OkHttpClientFactory;
 import com.facebook.react.modules.network.OkHttpClientProvider;
 import com.facebook.soloader.SoLoader;
 
-import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
-
 import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.security.ProviderInstaller;
 import com.google.android.gms.security.ProviderInstaller.ProviderInstallListener;
 
-import io.sentry.RNSentryPackage;
+import com.google.firebase.FirebaseApp;
 
 import java.util.List;
 import java.io.IOException;
@@ -34,7 +30,12 @@ import okhttp3.OkHttpClient;
 import okhttp3.Response;
 import okhttp3.Request;
 
+/**
+ * Classe principale dell'applicazione Tonchan.
+ * Inizializza React Native, Firebase e configura l'intercettazione dell'User-Agent.
+ */
 public class MainApplication extends Application implements ReactApplication {
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -44,7 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here
+      // Aggiunta manuale di pacchetti non autolinkabili
       packages.add(new RNBackgroundFetchPackage());
       packages.add(new TurtleCoinPackage());
       return packages;
@@ -64,63 +65,84 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+
+    // 🔥 Inizializzazione Firebase necessaria per evitare crash
+    FirebaseApp.initializeApp(this);
+
+    // ⚙️ Aggiornamento del provider di sicurezza per compatibilità TLS
     upgradeSecurityProvider();
 
-    /* tonchan-vx.x.x */
+    // 🧭 Impostazione User-Agent personalizzato per tutte le richieste HTTP
     setUserAgent("tonchan-v1.2.3");
 
+    // 🚀 Inizializzazione del loader nativo di React Native
     SoLoader.init(this, /* native exopackage */ false);
   }
 
+  /**
+   * Imposta un interceptor personalizzato per modificare l'User-Agent
+   */
   public void setUserAgent(String userAgent) {
     OkHttpClientProvider.setOkHttpClientFactory(new UserAgentClientFactory(userAgent));
   }
 
+  /**
+   * Aggiorna il provider di sicurezza tramite Google Play Services
+   */
   private void upgradeSecurityProvider() {
     ProviderInstaller.installIfNeededAsync(this, new ProviderInstallListener() {
       @Override
       public void onProviderInstalled() {
+        Log.i("SecurityProvider", "Provider TLS aggiornato correttamente.");
       }
 
       @Override
       public void onProviderInstallFailed(int errorCode, Intent recoveryIntent) {
         GooglePlayServicesUtil.showErrorNotification(errorCode, MainApplication.this);
+        Log.e("SecurityProvider", "Fallito aggiornamento TLS provider: codice " + errorCode);
       }
     });
   }
 }
 
+/**
+ * Interceptor per modificare l'User-Agent nelle richieste HTTP
+ */
 class UserAgentInterceptor implements Interceptor {
 
-    String userAgent;
+  private final String userAgent;
 
-    public UserAgentInterceptor(String userAgent) {
-        this.userAgent = userAgent;
-    }
+  public UserAgentInterceptor(String userAgent) {
+    this.userAgent = userAgent;
+  }
 
-    @Override
-    public Response intercept(Interceptor.Chain chain) throws IOException {
-        Request originalRequest = chain.request();
-        Request correctRequest = originalRequest.newBuilder()
-            .removeHeader("User-Agent")
-            .addHeader("User-Agent", this.userAgent)
-            .build();
+  @Override
+  public Response intercept(Chain chain) throws IOException {
+    Request originalRequest = chain.request();
+    Request modifiedRequest = originalRequest.newBuilder()
+        .removeHeader("User-Agent")
+        .addHeader("User-Agent", this.userAgent)
+        .build();
 
-        return chain.proceed(correctRequest);
-    }
+    return chain.proceed(modifiedRequest);
+  }
 }
 
+/**
+ * Factory per creare un client HTTP con interceptor personalizzato
+ */
 class UserAgentClientFactory implements OkHttpClientFactory {
 
-    String userAgent;
+  private final String userAgent;
 
-    public UserAgentClientFactory(String userAgent) {
-        this.userAgent = userAgent;
-    }
+  public UserAgentClientFactory(String userAgent) {
+    this.userAgent = userAgent;
+  }
 
-    @Override
-    public OkHttpClient createNewNetworkModuleClient() {
-        return com.facebook.react.modules.network.OkHttpClientProvider.createClientBuilder()
-                  .addInterceptor(new UserAgentInterceptor(this.userAgent)).build();
-    }
+  @Override
+  public OkHttpClient createNewNetworkModuleClient() {
+    return OkHttpClientProvider.createClientBuilder()
+        .addInterceptor(new UserAgentInterceptor(this.userAgent))
+        .build();
+  }
 }
